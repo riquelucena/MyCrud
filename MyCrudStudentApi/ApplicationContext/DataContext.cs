388e555ec41b005abda448134dff2bcb2c00ca69
Luiz Henrique Lucena <118210935+riquelucena@users.noreply.github.com>
2023-03-06 12:17:24 -0300

    Add files via upload

---
+++ MyCrudStudentApi/ApplicationContext/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCrudStudentApi.Models;
+
+namespace MyCrudStudentApi.ApplicationContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().HasKey(s => s.Id);
+        }
+
+        public DbSet<Student> students { get; set; }
+    }
+}
